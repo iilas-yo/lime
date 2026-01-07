c75792d9252c30f1afbdd51a3d20b19cb27f7e5e
iilas <251815282+iilas-yo@users.noreply.github.com>
2026-01-08 00:24:53 +0300

    Revert "Update Windows.hx"
    This reverts commit b8e3e30073e6e5cdebd7717430ecfc560120e073.

--- src/lime/system/Windows.hx
+++ src/lime/system/Windows.hx
@@ -234,8 +234,9 @@ class Windows {
 			DwmEnableBlurBehindWindow(hwnd, &blurBehind);
 
 			if (transparent) {
-				exStyle |= WS_EX_TRANSPARENT;
+				exStyle |= WS_EX_LAYERED | WS_EX_TRANSPARENT;
 			} else {
+				exStyle &= ~WS_EX_LAYERED;
 				exStyle &= ~WS_EX_TRANSPARENT;
 			}
 
